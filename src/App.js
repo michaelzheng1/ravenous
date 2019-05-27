ed39e8e23315ca6f7a9109eedc9d82f084f6b6aa
michaelzheng1 <michaelzheng0128@gmail.com>
2019-05-27 16:48:15 -0400

    adding the second part of ravenous

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-// import BusinessList from '../components/Business';
-// import SearchBar from '../components/SearchBar';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 
-function App() {
-  return (
-    <div class="App">
-  <h1>ravenous</h1>
-  <SearchBar/>
-  <BusinessList/>
-</div>
-  );
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business,
+]
+
+class App extends React.Component {
+  render() {
+    return (
+      <div class="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList businesses={businesses} />
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,14 +4,13 @@ import Business from '../Business/Business'
 
 class BusinessList extends React.Component {
     render() {
-        return(
- <div className="BusinessList">
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
+        return (
+            <div className="BusinessList">
+                {
+                    this.props.businesses.map((business) => {
+                        return <Business business={business}/>
+                    })
+                }
         </div>
         );
     }
