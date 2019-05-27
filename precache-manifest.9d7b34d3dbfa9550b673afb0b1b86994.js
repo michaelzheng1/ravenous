2d1311f447aad0b1bd4aabf9af5d5055bfbf6778
michaelzheng1 <michaelzheng0128@gmail.com>
2019-05-26 21:20:20 -0400

    Updates

---
+++ precache-manifest.9d7b34d3dbfa9550b673afb0b1b86994.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8702d119df32a7a84b5648630f31dde",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "637f65d768b4fa152c77",
+    "url": "/ravenous/static/css/main.48556961.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/ravenous/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "637f65d768b4fa152c77",
+    "url": "/ravenous/static/js/main.bdf4f8d5.chunk.js"
+  },
+  {
+    "revision": "1444c97d4f0ed3607f27",
+    "url": "/ravenous/static/js/runtime~main.f8ce170b.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
