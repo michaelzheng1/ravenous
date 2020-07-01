443b1c46fb49c81dd24777606902c4f3194ada5c
michaelzheng1 <mzheng1@terpmail.umd.edu>
2020-07-01 11:03:28 -0400

    fixing some lint error

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import React from 'react';
-import './App.css';
-import BusinessList from './components/BusinessList/BusinessList';
-import SearchBar from './components/SearchBar/SearchBar';
-import Yelp from './util/Yelp';
+import React from 'react'
+import './App.css'
+import BusinessList from './components/BusinessList/BusinessList'
+import SearchBar from './components/SearchBar/SearchBar'
+import Yelp from './util/Yelp'
 require('dotenv').config()
 
-
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      businesses: [],
-    };
-    this.searchYelp = this.searchYelp.bind(this);
+      businesses: []
+    }
+    this.searchYelp = this.searchYelp.bind(this)
   }
 
-  searchYelp(term, location, sortBy) {
+  searchYelp (term, location, sortBy) {
     Yelp.searchYelp(term, location, sortBy).then((businesses) => {
-      this.setState({businesses: businesses});
+      this.setState({ businesses: businesses })
     })
   }
 
-  render() {
+  render () {
     return (
-      <div class="App">
+      <div class='App'>
         <h1>ravenous</h1>
-        <SearchBar searchYelp={this.searchYelp}/>
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={this.state.businesses} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
