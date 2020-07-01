3b61052b1c2b3428930ef734596d3b77cfd8ea49
Michael Zheng <mzheng1@users.noreply.github.com>
2020-07-01 19:28:17 +0000

    Automated report

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -4,7 +4,7 @@ import './Business.css'
 class Business extends React.Component {
   render () {
     return (
-      <div className=                 'Business'>
+      <div className='Business'>
         <div className='image-container'>
           <img src={this.props.business.imageSrc} alt='' />
         </div>
