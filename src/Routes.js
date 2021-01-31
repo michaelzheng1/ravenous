fa9af0162988b8724e911645be5b4f282aea9733
michaelzheng1 <mzheng1@terpmail.umd.edu>
2021-01-30 22:39:07 -0500

    inital saga redux

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+const Routes = () => {
+    return (
+
+        <div className='App'>
+            <h1>ravenous</h1>
+            {/* <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} /> */}
+        </div>
+
+    )
+}
+
+export default Routes;
---
+++ src/redux/reducer/Types.js
@@ -0,0 +1,24 @@
+export default {
+  fetchData: "FETCH_DATA",
+  fetchDataFail: "FETCH_DATA_FAIL",
+  setData: "SET_DATA",
+  setPage: "SET_PAGE",
+  setViewMore: "SET_VIEW_MORE",
+  viewMore: "VIEW_MORE",
+
+  getErrors: "GET_ERRORS",
+  userLoading: "USER_LOADING",
+  setCurrentUser: "SET_CURRENT_USER",
+  registerUser: "REGISTER_USER",
+  getProfile: "GET_PROFILE",
+  logoutRequest: "LOGOUT_REQUEST",
+  resetError: "RESET_ERROR",
+  reset: "RESET",
+  logout: "LOGOUT",
+  requestWithToken: "REQUEST_WITH_TOKEN",
+  requestWithoutToken: "REQUEST_WITHOUT_TOKEN",
+  requestMessage: "REQUEST_MESSAGE",
+  messageResp: "MESSAGE_RESP",
+  loginUser: "LOGIN_USER",
+  loginSuccess: "LOGIN_SUCCESS"
+};
